7c95cf248e6b704718f825ffa6338a7b4571725d
vinayreddy17 <86202714+vinayreddy17@users.noreply.github.com>
2024-03-09 10:00:15 +0530

    Add files via upload

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+function Dashboard() {
+  return (
+    <div>
+      <Navbar />
+      Dashboard
+      </div>
+  )
+}
+
+export default Dashboard
